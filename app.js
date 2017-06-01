5e2472e35074c8fb606d111d3ee3c8faac335ce0
dobiedad <leomdiv@gmail.com>
2017-06-01 16:14:36 +0100

    lint

--- app.js
+++ app.js
@@ -7,40 +7,40 @@ var routes = {
 }
 
 function App (model) {
-   this.model = model
+  this.model = model
 }
 
-App.prototype.routes = function() {
+App.prototype.routes = function () {
   var self = this
 
-    return [
-      routes.root({
-       redirect: function() {
-         self.model.screen = 'home'
-       }
-      }),
-      routes.app({
-       bindings:{
-         screen: [self.model,'screen']
-       },
-       render: function() {
-         return self.renderMain(model)
-       }
-     })
-   ]
+  return [
+    routes.root({
+      redirect: function () {
+        self.model.screen = 'home'
+      }
+    }),
+    routes.app({
+      bindings: {
+        screen: [self.model, 'screen']
+      },
+      render: function () {
+        return self.renderMain(model)
+      }
+    })
+  ]
 }
 
-App.prototype.renderMain = function() {
-  var self = this;
+App.prototype.renderMain = function () {
+  var self = this
   return h('div.main',
-    h('h4','current screen: ' + this.model.screen),
-    h('button',{onclick:function () {
+    h('h4', 'current screen: ' + this.model.screen),
+    h('button', {onclick: function () {
       self.model.screen = 'apples'
-    }},'Change Sceren To Apples')
+    }}, 'Change Sceren To Apples')
   )
 }
 
-var model = {screen:'home'}
+var model = {screen: 'home'}
 var app = new App(model)
 
 hyperdom.append(document.body, app, {router: router})
